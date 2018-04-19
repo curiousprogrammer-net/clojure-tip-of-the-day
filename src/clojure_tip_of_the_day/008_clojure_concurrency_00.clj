4fd437aa2577f00d0782816bb05d60da3109016f
Juraj Martinka <jumarko@gmail.com>
2018-04-19 11:21:05 +0200

    Episode 8: Clojure Concurrency - Welcome.

---
+++ src/clojure_tip_of_the_day/008_clojure_concurrency_00.clj
@@ -0,0 +1,12 @@
+(ns clojure-tip-of-the-day.008-clojure-concurrency-00)
+
+;;; This is an introductory episode to the whole series
+;;; about Clojure concurrency.
+;;; We'll explore Clojure's concurrency primitives & tools
+;;; for dealing with state and concurrency such as
+;;; atoms, refs, agents, vars, futures, promises, delays and more
+;;;
+;;; ## Resources:
+;;;  * The Ultimate Guide to Clojure Concurrency: https://purelyfunctional.tv/guide/clojure-concurrency/
+;;;  * PurelyFunctional.tv concurrency lessons: https://purelyfunctional.tv/courses/concurrency/
+;;;  * Clojure Concurrency - Rich Hickey: https://www.youtube.com/watch?v=nDAfZK8m5_8
